93fcb0678b2a8f270ed373ec85b09f9e7f3404ef
andrewkorepanov <me@gentoo.arpanet>
2020-04-26 01:37:12 +0200

    user .config

---
+++ .config/cmus/lib.pl
@@ -0,0 +1,304 @@
+/home/me/music/Classic music/Hendel George Frederick/adagio_e_staccato.mp3
+/home/me/music/Classic music/Hendel George Frederick/affettuoso.mp3
+/home/me/music/Classic music/Hendel George Frederick/air.mp3
+/home/me/music/Classic music/Hendel George Frederick/allegro-alla_hornpipe.mp3
+/home/me/music/Classic music/Hendel George Frederick/allegro-andante-allegro.mp3
+/home/me/music/The Doors/Door6701.mp3
+/home/me/music/The Doors/Door6702.mp3
+/home/me/music/The Doors/Door6703.mp3
+/home/me/music/The Doors/Door6704.mp3
+/home/me/music/The Doors/Door6705.mp3
+/home/me/music/The Doors/Door6706.mp3
+/home/me/music/The Doors/Door6707.mp3
+/home/me/music/The Doors/Door6708.mp3
+/home/me/music/The Doors/Door6709.mp3
+/home/me/music/The Doors/Door6710.mp3
+/home/me/music/The Doors/Door6711.mp3
+/home/me/music/The Doors/Door6712.mp3
+/home/me/music/The Doors/Door6713.mp3
+/home/me/music/The Doors/Door6714.mp3
+/home/me/music/The Doors/Door6715.mp3
+/home/me/music/The Doors/Door6716.mp3
+/home/me/music/The Doors/Door6717.mp3
+/home/me/music/The Doors/Door6718.mp3
+/home/me/music/The Doors/Door6719.mp3
+/home/me/music/The Doors/Door6720.mp3
+/home/me/music/The Doors/Door6721.mp3
+/home/me/music/The Doors/Door6801.mp3
+/home/me/music/The Doors/Door6802.mp3
+/home/me/music/The Doors/Door6803.mp3
+/home/me/music/The Doors/Door6804.mp3
+/home/me/music/The Doors/Door6805.mp3
+/home/me/music/The Doors/Door6806.mp3
+/home/me/music/The Doors/Door6807.mp3
+/home/me/music/The Doors/Door6808.mp3
+/home/me/music/The Doors/Door6809.mp3
+/home/me/music/The Doors/Door6810.mp3
+/home/me/music/The Doors/Door6811.mp3
+/home/me/music/The Doors/Door6901.mp3
+/home/me/music/The Doors/Door6902.mp3
+/home/me/music/The Doors/Door6903.mp3
+/home/me/music/The Doors/Door6904.mp3
+/home/me/music/The Doors/Door6905.mp3
+/home/me/music/The Doors/Door6906.mp3
+/home/me/music/The Doors/Door6907.mp3
+/home/me/music/The Doors/Door6908.mp3
+/home/me/music/The Doors/Door6909.mp3
+/home/me/music/The Doors/Door7001.mp3
+/home/me/music/The Doors/Door7002.mp3
+/home/me/music/The Doors/Door7003.mp3
+/home/me/music/The Doors/Door7004.mp3
+/home/me/music/The Doors/Door7005.mp3
+/home/me/music/The Doors/Door7006.mp3
+/home/me/music/The Doors/Door7007.mp3
+/home/me/music/The Doors/Door7008.mp3
+/home/me/music/The Doors/Door7009.mp3
+/home/me/music/The Doors/Door7010.mp3
+/home/me/music/The Doors/Door7011.mp3
+/home/me/music/The Doors/Door7012.mp3
+/home/me/music/The Doors/Door7013.mp3
+/home/me/music/The Doors/Door7014.mp3
+/home/me/music/The Doors/Door7015.mp3
+/home/me/music/The Doors/Door7016.mp3
+/home/me/music/The Doors/Door7017.mp3
+/home/me/music/The Doors/Door7018.mp3
+/home/me/music/The Doors/Door7019.mp3
+/home/me/music/The Doors/Door7020.mp3
+/home/me/music/The Doors/Door7021.mp3
+/home/me/music/The Doors/Door7022.mp3
+/home/me/music/The Doors/Door7023.mp3
+/home/me/music/The Doors/Door7024.mp3
+/home/me/music/The Doors/Door7025.mp3
+/home/me/music/The Doors/Door7026.mp3
+/home/me/music/The Doors/Door7027.mp3
+/home/me/music/The Doors/Door7028.mp3
+/home/me/music/The Doors/Door7029.mp3
+/home/me/music/The Doors/Door7030.mp3
+/home/me/music/The Doors/Door7031.mp3
+/home/me/music/The Doors/Door7032.mp3
+/home/me/music/The Doors/Door7033.mp3
+/home/me/music/The Doors/Door7034.mp3
+/home/me/music/The Doors/Door7101.mp3
+/home/me/music/The Doors/Door7102.mp3
+/home/me/music/The Doors/Door7103.mp3
+/home/me/music/The Doors/Door7104.mp3
+/home/me/music/The Doors/Door7105.mp3
+/home/me/music/The Doors/Door7106.mp3
+/home/me/music/The Doors/Door7107.mp3
+/home/me/music/The Doors/Door7108.mp3
+/home/me/music/The Doors/Door7109.mp3
+/home/me/music/The Doors/Door7110.mp3
+/home/me/music/The Doors/Door8001.mp3
+/home/me/music/The Doors/Door8002.mp3
+/home/me/music/The Doors/Door8003.mp3
+/home/me/music/The Doors/Door8004.mp3
+/home/me/music/The Doors/Door8005.mp3
+/home/me/music/The Doors/Door8006.mp3
+/home/me/music/The Doors/Door8007.mp3
+/home/me/music/The Doors/Door8008.mp3
+/home/me/music/The Doors/Door8009.mp3
+/home/me/music/The Doors/Door8010.mp3
+/home/me/music/The Doors/Door8011.mp3
+/home/me/music/The Doors/Door8012.mp3
+/home/me/music/The Doors/Door9101.mp3
+/home/me/music/The Doors/Door9102.mp3
+/home/me/music/The Doors/Door9103.mp3
+/home/me/music/The Doors/Door9104.mp3
+/home/me/music/The Doors/Door9105.mp3
+/home/me/music/The Doors/Door9106.mp3
+/home/me/music/The Doors/Door9107.mp3
+/home/me/music/The Doors/Door9108.mp3
+/home/me/music/The Doors/Door9109.mp3
+/home/me/music/The Doors/Door9110.mp3
+/home/me/music/The Doors/Door9111.mp3
+/home/me/music/The Doors/Door9112.mp3
+/home/me/music/The Doors/Door9113.mp3
+/home/me/music/The Doors/Door9114.mp3
+/home/me/music/The Doors/Door9115.mp3
+/home/me/music/The Doors/Door9116.mp3
+/home/me/music/The Doors/Door9117.mp3
+/home/me/music/The Doors/Door9118.mp3
+/home/me/music/The Doors/Door9119.mp3
+/home/me/music/The Doors/Door9120.mp3
+/home/me/music/The Doors/Door9121.mp3
+/home/me/music/The Doors/Door9122.mp3
+/home/me/music/The Doors/Door9123.mp3
+/home/me/music/The Doors/Door9124.mp3
+/home/me/music/The Doors/Door9125.mp3
+/home/me/music/The Doors/Door9126.mp3
+/home/me/music/The Doors/Door9127.mp3
+/home/me/music/The Doors/Door9128.mp3
+/home/me/music/The Doors/Door9129.mp3
+/home/me/music/The Doors/Door9130.mp3
+/home/me/music/The Doors/Door9131.mp3
+/home/me/music/The Doors/Doors101.mp3
+/home/me/music/The Doors/Doors102.mp3
+/home/me/music/The Doors/Doors103.mp3
+/home/me/music/The Doors/Doors104.mp3
+/home/me/music/The Doors/Doors105.mp3
+/home/me/music/The Doors/Doors106.mp3
+/home/me/music/The Doors/Doors107.mp3
+/home/me/music/The Doors/Doors108.mp3
+/home/me/music/The Doors/Doors109.mp3
+/home/me/music/The Doors/Doors110.mp3
+/home/me/music/The Doors/Doors111.mp3
+/home/me/music/The Doors/Doors112.mp3
+/home/me/music/The Doors/Doors113.mp3
+/home/me/music/The Doors/Doors114.mp3
+/home/me/music/The Doors/Doors115.mp3
+/home/me/music/The Doors/Doors201.mp3
+/home/me/music/The Doors/Doors202.mp3
+/home/me/music/The Doors/Doors203.mp3
+/home/me/music/The Doors/Doors204.mp3
+/home/me/music/The Doors/Doors205.mp3
+/home/me/music/The Doors/Doors206.mp3
+/home/me/music/The Doors/Doors207.mp3
+/home/me/music/The Doors/Doors208.mp3
+/home/me/music/The Doors/Doors209.mp3
+/home/me/music/The Doors/Doors210.mp3
+/home/me/music/The Doors/Doors301.mp3
+/home/me/music/The Doors/Doors302.mp3
+/home/me/music/The Doors/Doors303.mp3
+/home/me/music/The Doors/Doors304.mp3
+/home/me/music/The Doors/Doors305.mp3
+/home/me/music/The Doors/Doors306.mp3
+/home/me/music/The Doors/Doors307.mp3
+/home/me/music/The Doors/Doors308.mp3
+/home/me/music/The Doors/Doors309.mp3
+/home/me/music/The Doors/Doors310.mp3
+/home/me/music/The Doors/Doors311.mp3
+/home/me/music/The Doors/Doors313.mp3
+/home/me/music/ИПВ/3.mp3
+/home/me/music/ИПВ/4.mp3
+/home/me/music/ИПВ/5.mp3
+/home/me/music/ИПВ/6.mp3
+/home/me/music/ИПВ/8.mp3
+/home/me/music/Classic music/Hendel George Frederick/allegro_moderato.mp3
+/home/me/music/Classic music/Mendelssohn-Bartholdy Felix/allegro_vivace.mp2
+/home/me/music/Classic music/Mendelssohn-Bartholdy Felix/andante_con_moto.mp2
+/home/me/music/Classic music/Hendel George Frederick/andante.mp3
+/home/me/music/Classic music/Hendel George Frederick/a_tempo_di_menuetto.mp3
+/home/me/music/Classic music/Hendel George Frederick/bourree_-_2.mp3
+/home/me/music/Classic music/Hendel George Frederick/bourree_-_3.mp3
+/home/me/music/Classic music/Hendel George Frederick/bourree.mp3
+/home/me/music/Classic music/Hendel George Frederick/cantabile.mp3
+/home/me/music/Classic music/Mendelssohn-Bartholdy Felix/con_moto_moderato.mp2
+/home/me/music/Classic music/Hendel George Frederick/coro.mp3
+/home/me/music/Classic music/Mendelssohn-Bartholdy Felix/dance_of_the_clowns_op_61-11.mp2
+/home/me/music/Classic music/Hendel George Frederick/hornpipe.mp3
+/home/me/music/Classic music/Hendel George Frederick/la_paix__largo_alla_siciliana_.mp3
+/home/me/music/Classic music/Hendel George Frederick/la_rejouissance__allegro_.mp3
+/home/me/music/Classic music/Hendel George Frederick/loure.mp3
+/home/me/music/Classic music/Hendel George Frederick/manuett.mp3
+/home/me/music/Classic music/Hendel George Frederick/menuett_-_2.mp3
+/home/me/music/Classic music/Hendel George Frederick/menuett_-_3.mp3
+/home/me/music/Classic music/Hendel George Frederick/menuett.mp3
+/home/me/music/Classic music/Hendel George Frederick/overture_-_2.mp3
+/home/me/music/Classic music/Hendel George Frederick/overture.mp3
+/home/me/music/Classic music/Hendel George Frederick/rigaudon.mp3
+/home/me/music/Classic music/Beethoven Ludwig Van/concerto_f_piano_and_orchestra_n1_op_15_-_3_rondo.mp3
+/home/me/music/Classic music/Beethoven Ludwig Van/piano_sonata.mp3
+/home/me/music/Classic music/Beethoven Ludwig Van/piano_sonata_n14__moonlight__op_27_n2_-1_adagio_sostenuto.mp3
+/home/me/music/Classic music/Beethoven Ludwig Van/symphonie_n5_op_67_-_1_allegro_con_brio.mp3
+/home/me/music/Classic music/Beethoven Ludwig Van/symphonie_n5_op_67_-_2_andante_con_moto.mp3
+/home/me/music/Classic music/Beethoven Ludwig Van/symphonie_n5_op_67_-_3-4_allegro.mp3
+/home/me/music/Classic music/Beethoven Ludwig Van/symphonie_n6_op_68_-_1_pleasant_thoughts_on_a.mp3
+/home/me/music/Classic music/Bach Johann Sebastian/brandenburg_concerto_n3_bwv_1048_-_1_allegro.mp3
+/home/me/music/Classic music/Bach Johann Sebastian/brandenburg_concerto_n3_bwv_1048_-_2_andante.mp3
+/home/me/music/Classic music/Bach Johann Sebastian/brandenburg_concerto_n3_bwv_1048_-_3_allegro.mp3
+/home/me/music/Classic music/Bach Johann Sebastian/cantata_n202_bwv_202.mp3
+/home/me/music/Classic music/Bach Johann Sebastian/chorale_prelude_bwv_140.mp3
+/home/me/music/Classic music/Bach Johann Sebastian/concerto_f_2_violins_bwv_1043_-_i_allegro.mp3
+/home/me/music/Classic music/Bach Johann Sebastian/concerto_f_2_violins_bwv_1043_-_iii_allegro.mp3
+/home/me/music/Classic music/Bach Johann Sebastian/concerto_f_2_violins_bwv_1043_-_ii_largo.mp3
+/home/me/music/Classic music/Bach Johann Sebastian/concerto_f_flute_violin_harpsichord_bwv_1044_-_i_allegr.mp3
+/home/me/music/Classic music/Bach Johann Sebastian/concerto_f_flute_violin_harpsichord_bwv_1044_-_ii_adari.mp3
+/home/me/music/Classic music/Bach Johann Sebastian/concerto_f_flute_violin_harpsichord_bwv_1044_-_iii_alla.mp3
+/home/me/music/Classic music/Bach Johann Sebastian/concerto_f_harpsichord_n5_bwv_1056_-_i_allegro.mp3
+/home/me/music/Classic music/Bach Johann Sebastian/concerto_f_harpsichord_n5_bwv_1056_-_iii_allegro.mp3
+/home/me/music/Classic music/Bach Johann Sebastian/concerto_f_harpsichord_n5_bwv_1056_-_ii_largo.mp3
+/home/me/music/Classic music/Hendel George Frederick/suite_n3_bwv_1068_-_aria.mp3
+/home/me/music/Classic music/Bach Johann Sebastian/toccata_bwv_565.mp3
+/home/me/music/Neil Young/Dead Man/01 Guitar Solo, No. 1.mp3
+/home/me/music/Neil Young/Dead Man/02 The Round Stones Beneath The Earth.mp3
+/home/me/music/Neil Young/Dead Man/03 Guitar Solo, No. 2.mp3
+/home/me/music/Neil Young/Dead Man/04 Why Does Thou Hide Thyself, Clouds.mp3
+/home/me/music/Neil Young/Dead Man/05 Organ Solo.mp3
+/home/me/music/Neil Young/Dead Man/06 Do You Know How To Use This Weapon.mp3
+/home/me/music/Neil Young/Dead Man/07 Guitar Solo, No. 3.mp3
+/home/me/music/Neil Young/Dead Man/08 Nobody's Story.mp3
+/home/me/music/Neil Young/Dead Man/09 Guitar Solo, No. 4.mp3
+/home/me/music/Neil Young/Dead Man/10 Stupid White Men.mp3
+/home/me/music/Neil Young/Dead Man/11 Guitar Solo, No. 5.mp3
+/home/me/music/Neil Young/Dead Man/12 Time For You To Leave, William Blake.mp3
+/home/me/music/Neil Young/Dead Man/13 Guitar Solo, No. 6.mp3
+/home/me/music/Classic music/Liszt Ference/concerto_f_piano_and_orchestra_n1_s124_-_i_allegro_maest.mp3
+/home/me/music/Classic music/Liszt Ference/concerto_f_piano_and_orchestra_n1_s124_-_ii_quasi_adagio.mp3
+/home/me/music/Classic music/Liszt Ference/hungarian_rhapsody_n6_s358.mp3
+/home/me/music/Classic music/Liszt Ference/love_dreams_n3_s541.mp3
+/home/me/music/Classic music/Liszt Ference/march-racotsi_s117.mp3
+/home/me/music/Classic music/Liszt Ference/prelude_-_symphonic_poem_s97.mp3
+/home/me/music/Classic music/Liszt Ference/sonnet_n104.mp3
+/home/me/music/Classic music/Chopin Frederick/barcarolle_op_60.mp3
+/home/me/music/Classic music/Chopin Frederick/etude_op_10_n3.mp3
+/home/me/music/Classic music/Chopin Frederick/fantasy-impromptu_op_66.mp3
+/home/me/music/Classic music/Chopin Frederick/polonaise_op_40_n1.mp3
+/home/me/music/Classic music/Chopin Frederick/prelude_op_28_n15.mp3
+/home/me/music/Classic music/Chopin Frederick/prelude_op_28_n7.mp3
+/home/me/music/Classic music/Chopin Frederick/prelude__transcription_f_piano_trio__op_28_n4.mp3
+/home/me/music/Classic music/Chopin Frederick/sonata_f_piano_n2_op_35_-_i_allegro_maestoso.mp3
+/home/me/music/Classic music/Chopin Frederick/sonata_f_piano_n2_op_35_-_iii_marcia_funibre.mp3
+/home/me/music/Classic music/Chopin Frederick/sonata_f_piano_n2_op_35_-_ii_scherzo.mp3
+/home/me/music/Classic music/Chopin Frederick/sonata_f_piano_n2_op_35.mp3
+/home/me/music/Classic music/Chopin Frederick/waltz_in_e_major.mp3
+/home/me/music/Classic music/Chopin Frederick/waltz_op_34_n2.mp3
+/home/me/music/Classic music/Chopin Frederick/waltz_op_64_n1.mp3
+/home/me/music/Classic music/Chopin Frederick/waltz_op_64_n2.mp3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/05.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/06.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/07.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/08.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/09.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/10.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/11.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/12.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/13.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/14.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/15.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/16.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/17.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/18.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/19.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/20.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/21.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/22.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/23.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/24.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/25.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/26.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/27.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/28.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/29.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/30.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/31.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/32.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/33.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/34.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/35.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/36.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/37.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/38.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/39.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/40.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/41.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/42.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/43.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/44.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/45.MP3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/00-2.mp3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/00-3.mp3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/01.mp3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/02.mp3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/03.mp3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/04.mp3
+/home/me/music/Аллен Карр/Легкий способ бросить курить/00-1.mp3
---
+++ .config/nvim/init.vim
@@ -0,0 +1,350 @@
+
+" vim-plug 
+
+call plug#begin(stdpath('data').'/plugged')
+
+Plug 'gentoo/gentoo-syntax'
+Plug 'tomasiser/vim-code-dark'
+
+Plug 'vim-airline/vim-airline'
+Plug 'vim-airline/vim-airline-themes'
+
+"Plug 'vifm/vifm.vim'
+Plug 'preservim/nerdcommenter'
+Plug 'preservim/nerdtree'
+Plug 'tiagofumo/vim-nerdtree-syntax-highlight'
+
+Plug 'neovim/nvim-lsp'
+" Plug 'neoclide/coc.nvim', {'branch': 'release'}
+" Plug 'puremourning/vimspector'
+" Plug 'sakhnik/nvim-gdb', { 'do': ':!./install.sh \| UpdateRemotePlugins' }
+
+Plug 'Yggdroot/indentLine'
+
+"Plug 'skywind3000/asyncrun.vim'
+
+call plug#end()
+
+
+
+" syntax highlight
+filetype plugin on
+syntax on
+
+" mouse support
+set mouse=a
+
+" line numbering
+set number
+
+" dark scheme
+set background=dark
+
+" trailing spaces/tabs
+set list
+set listchars=tab:→\ ,trail:·,extends:»,precedes:«,nbsp:¤
+
+" show a visual line under the cursor's current line
+set cursorline
+
+" enable folding
+set foldmethod=indent
+set foldlevel=99
+" space open/closes folds
+nnoremap <space> za
+
+" set tabs to have 4 spaces
+set tabstop=4
+set softtabstop=4
+" expand tabs into spaces
+set expandtab
+" indent when moving to the next line while writing code
+set autoindent
+" when using the >> or << commands, shift lines by 4 spaces
+set shiftwidth=4
+
+" color scheme
+" codedark: https://github.com/tomasiser/vim-code-dark#installation
+let g:codedark_term256=1
+colorscheme codedark
+
+" visual autocomplete for command menu
+set wildmenu
+set lazyredraw          " redraw only when we need to.
+set incsearch           " search as characters are entered
+set hlsearch            " highlight matches
+
+" enable all Python syntax highlighting features
+let python_highlight_all = 1
+highlight link pythonSpaceError NONE
+
+" Toggle spell checking on and off with `,s`
+nmap <silent> <leader>s :set spell!<CR>
+
+
+
+" rofi configuration syntax highlight
+au BufNewFile,BufRead /*.rasi setf css
+
+
+
+" Airline 
+" https://github.com/vim-airline/vim-airline
+" https://github.com/vim-airline/vim-airline-themes
+
+let g:airline#enable_at_startup = 1
+let g:airline#extensions#tabline#enabled = 1
+let g:airline#extensions#tabline#buffer_nr_show = 1
+let g:airline_theme='codedark'
+ 
+if !exists('g:airline_symbols')
+     let g:airline_symbols = {}
+endif
+ 
+" unicode symbols
+let g:airline_left_sep = '»'
+let g:airline_left_sep = ''
+let g:airline_right_sep = '«'
+let g:airline_right_sep = ''
+let g:airline_symbols.crypt = ''
+let g:airline_symbols.linenr = '☰'
+let g:airline_symbols.maxlinenr = ''
+let g:airline_symbols.branch = 'B'
+let g:airline_symbols.paste = 'ρ'
+let g:airline_symbols.spell = 'Ꞩ'
+let g:airline_symbols.notexists = 'Ɇ'
+let g:airline_symbols.whitespace = 'Ξ'
+let g:airline_symbols.readonly = '&'
+let g:airline_symbols.dirty='⚡'
+
+
+
+" indent line
+" https://github.com/Yggdroot/indentLine
+let g:indentLine_enabled = 1
+let g:indentLine_char = '┆'
+
+
+
+" vifm
+" https://github.com/vifm/vifm.vim
+"nnoremap <silent> <leader>n :Vifm<CR>
+
+
+
+" NERDTree
+" https://jdhao.github.io/2018/09/10/nerdtree_usage/
+let g:NERDTreeDirArrowExpandable = '▸'
+let g:NERDTreeDirArrowCollapsible = '▾'
+" side menu toggle
+nnoremap <silent> <leader>n :NERDTreeToggle<CR>
+" vim-nerdtree-syntax-highlight
+" https://github.com/tiagofumo/vim-nerdtree-syntax-highlight
+let g:NERDTreeFileExtensionHighlightFullName = 1
+let g:NERDTreeExactMatchHighlightFullName = 1
+let g:NERDTreePatternMatchHighlightFullName = 1
+let g:NERDTreeHighlightFolders = 1 " enables folder icon highlighting using exact match
+let g:NERDTreeHighlightFoldersFullName = 1 " highlights the folder name
+" you can add these colors to your .vimrc to help customizing
+let s:brown = "905532"
+let s:aqua =  "3AFFDB"
+let s:blue = "689FB6"
+let s:darkBlue = "44788E"
+let s:purple = "834F79"
+let s:lightPurple = "834F79"
+let s:red = "AE403F"
+let s:beige = "F5C06F"
+let s:yellow = "F09F17"
+let s:orange = "D4843E"
+let s:darkOrange = "F16529"
+let s:pink = "CB6F6F"
+let s:salmon = "EE6E73"
+let s:green = "8FAA54"
+let s:lightGreen = "31B53E"
+let s:white = "FFFFFF"
+let s:rspec_red = 'FE405F'
+let s:git_orange = 'F54D27'
+
+let g:NERDTreeExtensionHighlightColor = {} " this line is needed to avoid error
+let g:NERDTreeExtensionHighlightColor['css'] = s:blue " sets the color of css files to blue
+
+let g:NERDTreeExactMatchHighlightColor = {} " this line is needed to avoid error
+let g:NERDTreeExactMatchHighlightColor['.gitignore'] = s:git_orange " sets the color for .gitignore files
+
+let g:NERDTreePatternMatchHighlightColor = {} " this line is needed to avoid error
+let g:NERDTreePatternMatchHighlightColor['.*_spec\.rb$'] = s:rspec_red " sets the color for files ending with _spec.rb
+"augroup nerdtree_open
+"    autocmd!
+"    autocmd VimEnter * NERDTree | wincmd p
+"augroup END
+
+
+
+" coc.vim
+" https://github.com/neoclide/coc.nvim/wiki/Install-coc.nvim
+let g:python3_host_prog = expand('/usr/bin/python3.6')
+
+" jsonc comments highlight
+autocmd FileType json syntax match Comment +\/\/.\+$+
+
+" TextEdit might fail if hidden is not set.
+set hidden
+
+" Some servers have issues with backup files, see #649.
+set nobackup
+set nowritebackup
+
+" Give more space for displaying messages.
+set cmdheight=2
+
+" Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
+" delays and poor user experience.
+set updatetime=300
+
+" Don't pass messages to |ins-completion-menu|.
+set shortmess+=c
+
+" Always show the signcolumn, otherwise it would shift the text each time
+" diagnostics appear/become resolved.
+set signcolumn=yes
+
+" Use tab for trigger completion with characters ahead and navigate.
+" NOTE: Use command ':verbose imap <tab>' to make sure tab is not mapped by
+" other plugin before putting this into your config.
+inoremap <silent><expr> <TAB>
+      \ pumvisible() ? "\<C-n>" :
+      \ <SID>check_back_space() ? "\<TAB>" :
+      \ coc#refresh()
+inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
+
+function! s:check_back_space() abort
+  let col = col('.') - 1
+  return !col || getline('.')[col - 1]  =~# '\s'
+endfunction
+
+" Use <c-space> to trigger completion.
+inoremap <silent><expr> <c-space> coc#refresh()
+
+" Use <cr> to confirm completion, `<C-g>u` means break undo chain at current
+" position. Coc only does snippet and additional edit on confirm.
+if has('patch8.1.1068')
+  " Use `complete_info` if your (Neo)Vim version supports it.
+  inoremap <expr> <cr> complete_info()["selected"] != "-1" ? "\<C-y>" : "\<C-g>u\<CR>"
+else
+  imap <expr> <cr> pumvisible() ? "\<C-y>" : "\<C-g>u\<CR>"
+endif
+
+" Use `[g` and `]g` to navigate diagnostics
+nmap <silent> [g <Plug>(coc-diagnostic-prev)
+nmap <silent> ]g <Plug>(coc-diagnostic-next)
+
+" GoTo code navigation.
+nmap <silent> gd <Plug>(coc-definition)
+nmap <silent> gy <Plug>(coc-type-definition)
+nmap <silent> gi <Plug>(coc-implementation)
+nmap <silent> gr <Plug>(coc-references)
+
+nmap <silent> rr <Plug>(coc-refactor)
+
+" Use K to show documentation in preview window.
+nnoremap <silent> K :call <SID>show_documentation()<CR>
+
+function! s:show_documentation()
+  if (index(['vim','help'], &filetype) >= 0)
+    execute 'h '.expand('<cword>')
+  else
+    call CocAction('doHover')
+  endif
+endfunction
+
+" Highlight the symbol and its references when holding the cursor.
+autocmd CursorHold * silent call CocActionAsync('highlight')
+
+" Symbol renaming.
+nmap <leader>rn <Plug>(coc-rename)
+
+" Formatting selected code.
+xmap <leader>f  <Plug>(coc-format)
+nmap <leader>f  <Plug>(coc-format)
+
+augroup mygroup
+  autocmd!
+  " Setup formatexpr specified filetype(s).
+  autocmd FileType typescript,json setl formatexpr=CocAction('formatSelected')
+  " Update signature help on jump placeholder.
+  autocmd User CocJumpPlaceholder call CocActionAsync('showSignatureHelp')
+augroup end
+
+" Applying codeAction to the selected region.
+" Example: `<leader>aap` for current paragraph
+xmap <leader>a  <Plug>(coc-codeaction-selected)
+nmap <leader>a  <Plug>(coc-codeaction-selected)
+
+" Remap keys for applying codeAction to the current line.
+nmap <leader>ac  <Plug>(coc-codeaction)
+" Apply AutoFix to problem on the current line.
+nmap <leader>qf  <Plug>(coc-fix-current)
+
+" Introduce function text object
+" NOTE: Requires 'textDocument.documentSymbol' support from the language server.
+xmap if <Plug>(coc-funcobj-i)
+xmap af <Plug>(coc-funcobj-a)
+omap if <Plug>(coc-funcobj-i)
+omap af <Plug>(coc-funcobj-a)
+
+" Use <TAB> for selections ranges.
+" NOTE: Requires 'textDocument/selectionRange' support from the language server.
+" coc-tsserver, coc-python are the examples of servers that support it.
+nmap <silent> <TAB> <Plug>(coc-range-select)
+xmap <silent> <TAB> <Plug>(coc-range-select)
+
+" Add `:Format` command to format current buffer.
+command! -nargs=0 Format :call CocAction('format')
+
+" Add `:Fold` command to fold current buffer.
+command! -nargs=? Fold :call     CocAction('fold', <f-args>)
+
+" Add `:OR` command for organize imports of the current buffer.
+command! -nargs=0 OR   :call     CocAction('runCommand', 'editor.action.organizeImport')
+
+" Add (Neo)Vim's native statusline support.
+" NOTE: Please see `:h coc-status` for integrations with external plugins that
+" provide custom statusline: lightline.vim, vim-airline.
+set statusline^=%{coc#status()}%{get(b:,'coc_current_function','')}
+
+" Mappings using CoCList:
+" Show all diagnostics.
+nnoremap <silent> <space>a  :<C-u>CocList diagnostics<cr>
+" Manage extensions.
+nnoremap <silent> <space>e  :<C-u>CocList extensions<cr>
+" Show commands.
+nnoremap <silent> <space>c  :<C-u>CocList commands<cr>
+" Find symbol of current document.
+nnoremap <silent> <space>o  :<C-u>CocList outline<cr>
+" Search workspace symbols.
+nnoremap <silent> <space>s  :<C-u>CocList -I symbols<cr>
+" Do default action for next item.
+nnoremap <silent> <space>j  :<C-u>CocNext<CR>
+" Do default action for previous item.
+nnoremap <silent> <space>k  :<C-u>CocPrev<CR>
+" Resume latest coc list.
+nnoremap <silent> <space>p  :<C-u>CocListResume<CR>
+
+
+
+" vimspector 
+let g:vimspector_enable_mappings = 'VISUAL_STUDIO'
+" Start
+"nmap <silent> <F5> <Plug>VimspectorContinue
+" Finish
+nmap <silent> <F12> :VimspectorReset<CR>
+" 
+nmap <silent> <F4> :VimspectorEval<CR>
+
+" Run python
+autocmd FileType python nmap <buffer> <F7> :w<CR>:AsyncRun -raw python %<CR>:copen<CR>
+autocmd FileType python imap <buffer> <F7> <esc>:w<CR>:AsyncRun -raw python %<CR>:copen<CR>
+
+
+
+
